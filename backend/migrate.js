f40948aefd2622efadb057f6fe29833f0d1dd0a4
ichavamp1 <blanarucristian224@gmail.com>
2023-12-26 14:17:21 +0200

    Update migration script

--- backend/migrate.js
+++ backend/migrate.js
@@ -11,16 +11,26 @@ const conn = mysql.createConnection({
 
 const users_table = `CREATE TABLE users(id int PRIMARY KEY AUTO_INCREMENT, username varchar(100) NOT NULL, password varchar(255) NOT NULL);`;
 const messages_table = `CREATE TABLE messages(id int PRIMARY KEY AUTO_INCREMENT, content varchar(255), user_id int, FOREIGN KEY(user_id) REFERENCES users(id));`;
-const rooms_table = `CREATE TABLE rooms(id int PRIMARY KEY AUTO_INCREMENT, code varchar(100) NOT NULL, password varchar(255) NULL, admin_id int, FOREIGN KEY(admin_id) REFERENCES Users(id));`;
+const rooms_table = `CREATE TABLE rooms(id int PRIMARY KEY AUTO_INCREMENT, code varchar(100) NOT NULL, name varchar(255), password varchar(255) NULL, admin_id int, FOREIGN KEY(admin_id) REFERENCES Users(id));`;
 const user_room_table = `CREATE TABLE user_room( id int PRIMARY KEY AUTO_INCREMENT, user_id int, room_id int, FOREIGN KEY(user_id) REFERENCES Users(id), FOREIGN KEY(room_id) REFERENCES Rooms(id));`;
-
-//for logouts
 const invalid_tokens = `CREATE TABLE invalid_tokens(id int PRIMARY KEY AUTO_INCREMENT, value TEXT, logged_at DATETIME DEFAULT NOW());`;
 
-promisifiedQuery(conn, users_table)
+const drop_users = `DROP TABLE users`;
+const drop_messages = `DROP TABLE messages`;
+const drop_rooms = `DROP TABLE rooms`;
+const drop_user_room = `DROP TABLE user_room`;
+const drop_invalid_tokens = `DROP TABLE invalid_tokens`;
+
+
+promisifiedQuery(conn, drop_invalid_tokens) //start dropping
+    .then(() => promisifiedQuery(conn, drop_messages))
+    .then(() => promisifiedQuery(conn, drop_user_room))
+    .then(() => promisifiedQuery(conn, drop_rooms))
+    .then(() => promisifiedQuery(conn, drop_users)) //stop dropping
+    .then(() => promisifiedQuery(conn, users_table)) // start creating
     .then(() => promisifiedQuery(conn, messages_table))
     .then(() => promisifiedQuery(conn, rooms_table))
     .then(() => promisifiedQuery(conn, user_room_table))
-    .then(() => promisifiedQuery(conn, invalid_tokens))
+    .then(() => promisifiedQuery(conn, invalid_tokens)) //stop creating
     .catch(error => console.log(error))
-    .finally(() => conn.end());
+    .finally(() => conn.end()); //close connection
